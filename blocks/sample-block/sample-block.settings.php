5f68b0e68a4f30c8d7631e6368039490a48cc663
José Debuchy <jdebuchy@40q.com.ar>
2019-03-20 16:51:27 -0300

    Clean files

--- blocks/sample-block/sample-block.settings.php
+++ blocks/sample-block/sample-block.settings.php
@@ -3,7 +3,7 @@
 return [
     'name' => 'sample-block',
     'title' => __('Sample Block'),
-    'description' => __('Sample Block.'),
+    'description' => __('Sample Block'),
     'render_callback' => 'ACF_Gutenberg\Lib\my_acf_block_render_callback',
     'category' => 'common',
     'icon' => 'admin-comments',
--- blocks/steps/index.blade.php
+++
@@ -1,7 +0,0 @@
-<section class="g-py-100">
-    <div class="container">
-        <div class="row">
-            Test
-        </div>
-    </div>
-</section>

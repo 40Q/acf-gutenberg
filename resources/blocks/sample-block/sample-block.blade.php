3aa11c9eb665cd960cd42f3cf6931c657c8c09f0
José Debuchy <jdebuchy@40q.com.ar>
2019-05-06 20:11:16 -0300

    Include component on file

--- resources/blocks/sample-block/sample-block.blade.php
+++ resources/blocks/sample-block/sample-block.blade.php
@@ -1,8 +1,8 @@
-<section id="{{ $block->id }}" class="{{ $block->class }}" >
+@mainwrapper(['block' => $block])
   <div class="container">
       <h2>{{ $block->title }}</h2>
       <p>{{ $block->text }}</p>
       <p>{{ $block->intro }}</p>
       <p>{{ $block->custom_prop }}</p>
   </div>
-</section>
+@endmainwrapper

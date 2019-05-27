2bbff7b26be9501e82afb282ca0ac01456a77c0b
Pablo Capello <capellopablo@gmail.com>
2019-05-26 23:38:21 -0300

    Form Block base

--- cli/_base/blocks_templates/acfgb-form/acfgb-form.blade.php
+++ cli/_base/blocks_templates/acfgb-form/acfgb-form.blade.php
@@ -1,27 +1,30 @@
 @wrapper(['block' => $block])
-
-    <div class="container">
-        @if(isset($block->title) || isset($block->intro))
+@container(['block' => $block])
+    @if(isset($block->title) || isset($block->intro))
         <div class="row">
             <div class="col-md-5 offset-md-1">
                 @isset( $block->title )
-                <h2 class="featured-title">{{ $block->title }}</h2>
+                    <h2 class="featured-title">{{ $block->title }}</h2>
                 @endisset
             </div>
             <div class="col-md-5">
                 @isset( $block->intro )
-                <p>{{ $block->intro }}</p>
+                    <p>{{ $block->intro }}</p>
                 @endisset
             </div>
         </div>
-        @endif
-        <div class="row">
-            <div class="col-md-10 offset-md-1">
-                @isset( $block->form_shortcode )
+    @endif
+    <div class="row">
+        <div class="col-md-10 offset-md-1">
+            @isset( $block->form_shortcode )
                 <?php echo do_shortcode($block->form_shortcode); ?>
-                @endisset
-            </div>
-        </div>
-    </div><!-- .container -->
+            @endisset
 
+            @isset( $block->form_shortcode_cf7 )
+                <?php echo do_shortcode($block->cf7); ?>
+            @endisset
+        </div>
+
+    </div>
+@endcontainer
 @endwrapper
--- cli/_base/blocks_templates/acfgb-form/acfgb-form.class.php
+++ cli/_base/blocks_templates/acfgb-form/acfgb-form.class.php
@@ -13,7 +13,7 @@ class AcfgbForm extends Block
 
     public function init()
     {
-        // Use this method in extended classes
+        $this->cf7 = $this->get_cf7_shortcode();
     }
 
     public function set_settings()
@@ -36,10 +36,73 @@ class AcfgbForm extends Block
                     'id' => 'acfgb-tab-content-'.$this->slug,
                 ]
             ])
-                ->addText('title')
-                ->addTextarea('intro')
-                ->addText('form_shortcode');
+            ->addText('title',[
+                'default_value' => 'Form Title'
+            ])
+            ->addTextarea('intro',[
+                'default_value' => $this->get_ipsum()
+            ])
+            ->addTrueFalse('custom_shortcode', [
+                'default_value' => 0,
+                'ui' => 1,
+            ])
+            ->addSelect('form_shortcode_cf7',[
+                'choices' => $this->get_cf7_forms(),
+                'conditional_logic' => [
+                    [
+                        [
+                            'field' => 'custom_shortcode',
+                            'operator' => '==',
+                            'value' => '0',
+                        ],
+                    ],
+                ]
+            ])
+            ->addText('form_shortcode',[
+                'default_value' => 'Paste shortcode',
+                'conditional_logic' => [
+                    [
+                        [
+                            'field' => 'custom_shortcode',
+                            'operator' => '==',
+                            'value' => '1',
+                        ],
+                    ],
+                ]
+            ]);
 
         $this->fields = $fields;
     }
+
+    public function get_cf7_forms()
+    {
+        $args = [
+            'posts_per_page' => -1,
+            'post_type' => 'wpcf7_contact_form',
+        ];
+
+        $cf7_forms_query = new \WP_Query($args);
+        $cf7_forms = [];
+        if ( $cf7_forms_query->have_posts() ) {
+            while ( $cf7_forms_query->have_posts() ) { $cf7_forms_query->the_post();
+                $cf7_forms[get_the_title().'|-|'.get_the_ID()] = get_the_title();
+            }
+            wp_reset_postdata();
+        } else {
+            $cf7_forms = null;
+        }
+
+        return $cf7_forms;
+
+    }
+    public function get_cf7_shortcode()
+    {
+        $shortcode = false;
+        if (isset($this->form_shortcode_cf7) && !empty($this->form_shortcode_cf7)){
+            $cf7_form = explode('|-|',$this->form_shortcode_cf7);
+            $shortcode = '[contact-form-7 id="'.$cf7_form[1].'" title="'.$cf7_form[0].'"].';
+        }
+        return $shortcode;
+    }
+
 }

2d0e9637e510425204a9040f58101c34ffad4f20
Pablo Capello <capellopablo@gmail.com>
2019-05-27 22:47:21 -0300

    Fix block base blade

--- cli/_base/_block-base/block_base.blade.php
+++ cli/_base/_block-base/block_base.blade.php
@@ -1,9 +1,9 @@
 @wrapper(['block' => $block])
 @container(['block' => $block])
-    <h2>{{ $block->title }}</h2>
-    <p>{{ $block->text }}</p>
-    <p>{{ $block->intro }}</p>
-    <p>{{ $block->custom_prop }}</p>
-    @button(['button' => $block->button])@endbutton
+    <h2>{{ $content['title'] }}</h2>
+    <p>{{ $content['text'] }}</p>
+    <p>{{ $content['intro'] }}</p>
+    <p>{{ $content['custom_prop'] }}</p>
+    @button(['block' => $block])@endbutton
 @endcontainer
 @endwrapper

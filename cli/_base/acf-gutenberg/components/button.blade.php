31acde223213a000b46f202fabdaebb4bd28fc63
Pablo Capello <capellopablo@gmail.com>
2019-05-28 00:13:27 -0300

    Button component

--- cli/_base/acf-gutenberg/components/button.blade.php
+++ cli/_base/acf-gutenberg/components/button.blade.php
@@ -1,3 +1,8 @@
-<a  href="{{ $block->content['button']['link'] }}"
-    class="btn {{ $block->content['button']['class'] }} {{ $block->custom_classes['button_class'] }}"
-    target="{{ $block->content['button']['target'] }}">{{ $block->content['button']['text'] }}</a>
+@if($block->content['button']['link'] && $block->content['button']['text'])
+    <a  href="{{ $block->content['button']['link'] }}"
+        class="btn {{ $block->content['button']['class'] }} {{ $block->custom_classes['button_class'] }}"
+        @if($block->content['button']['target'])
+            target="{{ $block->content['button']['target'] }}"
+        @endif
+    >{{ $block->content['button']['text'] }}</a>
+@endif

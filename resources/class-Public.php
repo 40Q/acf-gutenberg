2cbaf67ccbf1cbcd7320d29c31229aa2f61d8c4f
Pablo Capello <capellopablo@gmail.com>
2019-06-13 18:28:52 -0300

    Fix console error loadingpublic assets

--- resources/class-Public.php
+++ resources/class-Public.php
@@ -67,7 +67,7 @@ class ACF_Gutenberg_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/plugin-name-public.css', array(), $this->version, 'all' );
+		//wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/plugin-name-public.css', array(), $this->version, 'all' );
 
 	}
 
@@ -79,7 +79,7 @@ class ACF_Gutenberg_Public {
 	public function enqueue_scripts() {
 
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-public.js', array( 'jquery' ), $this->version, false );
+		//wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
